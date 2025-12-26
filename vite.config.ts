2cd100dfa24252419894687ee4321cc7f063924d
mynameistito <github@mynameistito.com>
2025-12-26 17:31:32 +1300

    removed husky, updated gitignore, implemented wrangler int

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       "@": "/src",
