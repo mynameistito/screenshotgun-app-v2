4ccc43bc9dea0b67d677465dd77bc4b504e4557d
mynameistito <github@mynameistito.com>
2025-12-26 16:20:08 +1300

    update dep, added ultracite, added wrangler, fixed up ultracite and tw4 related issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Outfit', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ["Outfit", "ui-sans-serif", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
@@ -52,4 +49,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
